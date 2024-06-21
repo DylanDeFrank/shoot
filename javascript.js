992365add8ce0060e2dc5a1c0c39c10045a5b952
Dylan DeFrank <defrankdylan@gmail.com>
2024-06-21 14:43:59 -0700

    Create function to store input from the user
    Needed to assign the variable 'answer' to the prompt in order to have it
    follow the comparison in the conditional statements.

--- javascript.js
+++ javascript.js
@@ -4,12 +4,22 @@ function getRandomInt() {
     return Math.floor(Math.random() * (maxFloored - minCeiled)) 
   
   }
-  function getComputerChoice() {
-      if (getRandomInt()== 0) {
-      return 'rock'
-      } else if (getRandomInt() == 1) {
-        return 'paper'
-      } else {
-        return 'scissors'  
-      }
-  }
+function getComputerChoice() {
+    if (getRandomInt()== 0) {
+    return 'rock'
+    } else if (getRandomInt() == 1) {
+    return 'paper'
+    } else {
+    return 'scissors'  
+    }
+}
+  let answer = prompt('Choose rock, paper, or scissors') 
+    if (answer == 'rock') {
+        alert('You have chosen rock!')
+    } else if (answer == 'paper') {
+        alert('You have chosen paper!')
+    } else if (answer == 'scissors') {
+        alert ('You have chosen scissors!')
+    } else {
+        alert ('Not recognized. Please input value exactly how it appears.')
+    }
