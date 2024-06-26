1c81e813098faf7afcb48c349465abc3d1abf031
Dylan DeFrank <defrankdylan@gmail.com>
2024-06-26 11:16:34 -0700

    Create a function that plays 5 rounds
    I put the playRound() function into playGame() while alerting the score
    at the end of each round. At the end of 5 rounds I added a comparison
    between the scores and announced a winner depending on the greater of
    the two.

--- javascript.js
+++ javascript.js
@@ -2,12 +2,12 @@ function getRandomInt() {
     const minCeiled = Math.ceil(0);
     const maxFloored = Math.floor(3);
     return Math.floor(Math.random() * (maxFloored - minCeiled)) 
-  
-  }
+}
 function getComputerChoice() {
-    if (getRandomInt()== 0) {
+    const choiceCheck = getRandomInt()
+    if (choiceCheck== 0) {
     return 'rock'
-    } else if (getRandomInt() == 1) {
+    } else if (choiceCheck == 1) {
     return 'paper'
     } else {
     return 'scissors'  
@@ -24,23 +24,56 @@ function getHumanChoice() {
         } else {
             return  ('Not recognized. Please input value exactly how it appears.')
         }
+}
+
+let humanScore = 0
+let computerScore = 0 
+
+function playRound() {
+    const humanChoice = getHumanChoice() 
+    const computerChoice = getComputerChoice()
+    if (humanChoice == 'rock' && computerChoice == 'paper') {
+        computerScore++ 
+        alert('You lose. Paper covers rock.')
+    } else if (humanChoice == 'rock' && computerChoice == 'scissors') {
+        humanScore++
+        alert('You win. Rock beats scissors.')
+    } else if (humanChoice == 'rock' && computerChoice == 'rock') {
+        alert('You tied.')
+    } 
+    if (humanChoice == 'paper' && computerChoice == 'rock') {
+        humanScore++
+        alert('You win. Paper covers rock.')
+    } else  if (humanChoice == 'paper' && computerChoice == 'paper') {
+        alert('You tied.')
+    } else if (humanChoice == 'paper' && computerChoice == 'scissors') {
+        computerScore++
+        alert('You lose. Scissors cuts paper.')
     }
+    if (humanChoice == 'scissors' && computerChoice == 'rock') {
+        computerScore++
+        alert('You lose. Rock beats scissors.')
+    } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
+        humanScore++
+        alert('You win. Scissors cuts paper.')
+    } else if (humanChoice == 'scissors' && computerChoice == 'scissors') {
+        alert('You tied.')
+    } return
+ }
 
-const humanScore = 0
-const computerScore = 0 
-
-const ComputerChoice = getComputerChoice()
-const HumanChoice = getHumanChoice()
-function playRound (HumanChoice, ComputerChoice) {
-    if (HumanChoice == 'rock' && ComputerChoice == 'paper') {
-        return 'You lose. Paper beats rock.'
-    } else if (HumanChoice == 'rock' && ComputerChoice == 'scissors') {
-        return 'You win. Rock beats scissors.'
-    } else {
-        return 'You tied.'
-    }
-
-
-    }
-
-console.log(playRound())
+ function playGame() {
+    playRound()
+      alert('Your score is ' + humanScore + ' and the computer score is ' + computerScore)
+    playRound()
+      alert('Your score is ' + humanScore + ' and the computer score is ' + computerScore)
+    playRound()
+      alert('Your score is ' + humanScore + ' and the computer score is ' + computerScore)
+    playRound()
+      alert('Your score is ' + humanScore + ' and the computer score is ' + computerScore)
+    playRound()
+      alert('Your score is ' + humanScore + ' and the computer score is ' + computerScore)
+    if (humanScore > computerScore)
+        alert('You win the game!')
+    else
+       alert('You lose the game!')
+ }
